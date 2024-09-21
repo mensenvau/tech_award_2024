d486662a95c7ea38fabeadfcd478b93acc24a6ee
NurmuhammadTukhtayev <nick02940@gmail.com>
2024-09-21 13:59:05 +0500

    add new tables to migration

--- jobs/main.py
+++ jobs/main.py
@@ -40,28 +40,150 @@ def insert_into_mysql(data):
             print("MySQL connected.")
             cursor = connection.cursor()
 
-            # Define MySQL insert query
-            insert_query = """
+            # Define MySQL insert queries
+            jobs_query = """
                 INSERT INTO jobs 
-                (is_job_vacancy, tg_message_id, job_name, company_name, job_type, job_salary, job_descriptions, input_language, jobs_date) 
+                (is_job_vacancy, msg_id, job_name, company_name, job_type, job_salary, job_descriptions, input_language, jobs_date) 
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
             """
+            
+            job_locations_query = """
+                INSERT INTO job_locations 
+                (job_id, country_code, country, city, full_address, is_remote_work) 
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """
+
+            contact_information_query = """
+                INSERT INTO contact_information 
+                (job_id, type, value) 
+                VALUES (%s, %s, %s)
+            """
+
+            job_responsibilities_query = """
+                INSERT INTO job_responsibilities 
+                (job_id, responsibility) 
+                VALUES (%s, %s)
+            """
+            requirement_skills_query = """
+                INSERT INTO requirement_skills 
+                (job_id, skill) 
+                VALUES (%s, %s)
+            """
+
+            occupations_skills_query = """
+                INSERT INTO occupations 
+                (job_id, occupation) 
+                VALUES (%s, %s)
+            """
+
+            additional_comments_query = """
+                INSERT INTO additional_comments 
+                (job_id, comment) 
+                VALUES (%s, %s)
+            """
+
+            search_keywords_query = """
+                INSERT INTO search_keywords 
+                (job_id, keyword) 
+                VALUES (%s, %s)
+            """
+
             print("Loading data into MySQL")
             # Insert each document from MongoDB into MySQL
-            for job in data:
-                print(f"{count + 1} {job['job_name']}")
+            for job_data in data:
+                # Assigning values to variables
+                is_job_vacancy = job_data["is_job_vacancy"]
+                message_id = str(job_data["message_id"])
+                job_name = job_data["job_name"]
+                company_name = job_data["company_name"]
+                job_location = job_data["job_location"]
+                job_type = job_data["job_type"]
+                job_salary = job_data["job_salary"]
+                job_descriptions = job_data["job_descriptions"]
+                job_responsibilities = job_data["job_responsibilities"]
+                requirement_skills = job_data["requirement_skills"]
+                occupations = job_data["occupations"]
+                additional_comments = job_data["additional_comments"]
+                contact_informations = job_data["contact_informations"]
+                input_language = job_data["input_language"]
+                search_keywords = job_data["search_keywords"]
+                createdAt = job_data["createdAt"]
+
+                # Extracting job location details into separate variables
+                job_country_code = job_location["country_code"]
+                job_country = job_location["country"]
+                job_city = job_location["city"]
+                job_full_address = job_location["full_address"]
+                job_is_remote_work = job_location["is_remote_work"]
+
+                print(f"{count + 1} {job_name}")
                 
-                cursor.execute(insert_query, (
-                    job['is_job_vacancy'],
-                    str(job['message_id']),
-                    job['job_name'],
-                    job['company_name'],
-                    job['job_type'],
-                    job['job_salary'],
-                    job['job_descriptions'],
-                    job['input_language'],
-                    job['createdAt']
+                # jobs table
+                cursor.execute(jobs_query, (
+                    is_job_vacancy,
+                    str(message_id),
+                    job_name,
+                    company_name,
+                    job_type,
+                    job_salary,
+                    job_descriptions,
+                    input_language,
+                    createdAt
                 ))
+                current_job_id = cursor.lastrowid
+
+                # locations table
+                cursor.execute(job_locations_query, (
+                        current_job_id,
+                        job_country_code,
+                        job_country,
+                        job_city,
+                        job_full_address,
+                        job_is_remote_work
+                ))
+                
+                # contact info
+                for info in contact_informations:
+                    cursor.execute(contact_information_query, (
+                        current_job_id,
+                        info['type'],
+                        info['value']
+                    ))
+
+                # info responsibilities
+                for info in job_responsibilities:
+                    cursor.execute(job_responsibilities_query, (
+                        current_job_id,
+                        info
+                    ))
+
+                # requirement skills
+                for info in requirement_skills:
+                    cursor.execute(requirement_skills_query, (
+                        current_job_id,
+                        info
+                    ))
+                
+                # occupations
+                for info in occupations:
+                    cursor.execute(occupations_skills_query, (
+                        current_job_id,
+                        info
+                    ))
+
+                # comments
+                for info in additional_comments:
+                    cursor.execute(additional_comments_query, (
+                        current_job_id,
+                        info
+                    ))
+                
+                # search keywords
+                for info in search_keywords:
+                    cursor.execute(search_keywords_query, (
+                        current_job_id,
+                        info
+                    ))
                 count += 1
 
             # Commit the transaction
