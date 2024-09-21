321df8dc2d08b9d18d58da4423c5be59677cb8a6
NurmuhammadTukhtayev <nick02940@gmail.com>
2024-09-21 23:41:13 +0500

    complete auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -8,20 +8,25 @@ const { smsSender } = require("uzdev/sender");
 // });
 
 exports.authSignUp = fnCatch(async (req, res, next) => {
-    let { phone_number } = req.body;
+    let { phone_number, first_name, last_name, birth_date } = req.body;
 
     let count = await execute("SELECT * FROM sms_code WHERE phone_num = ? and DATEDIFF(now(),savetime) <= 2", [phone_number]);
     if (count.length > 20) throw new Error("BLOCK 2 DAYS");
 
     let code = randomCode(5);
     let ins = await execute("insert into sms_code (phone_num, code) values (?, ?)", [phone_number, code]);
+    
+    let data = await execute("select * from users where phone_num = ?", [phone_number]);
+    if(!data.length){
+        let res = await execute("insert into users(phone_num, first_name, last_name, birth_date) values (?,?,?,?)", [phone_number, first_name, last_name, birth_date])
+        console.log(res);
+    }    
 
-    console.log(phone_number, code)
     smsSender(phone_number, `Jobgram tizimidan ro'yxatdan o'tish uchun kod: ${code}`);
     
     return res.json({
         message: "Ok",
-        token: enCode({ id: ins.insertId, type: "sms" }),
+        token: enCode({ id: ins.insertId, user_id:data[0].id || res.insertId, code, type: "sms" }),
     });
 });
 
@@ -38,7 +43,7 @@ exports.authSignIn = fnCatch(async (req, res, next) => {
 
     return res.json({
         message: "Ok",
-        token: enCode({ id: ins.insertId, type: "sms" }),
+        token: enCode({ id: ins.insertId, code, type: "sms" }),
     });
 });
 
@@ -46,14 +51,24 @@ exports.verifyPhoneCode = fnCatch(async (req, res, next) => {
     let {code} = req.body;
     let token = req.headers['token']
     
-        
+    let obj = deCode(token);
+    console.log(obj);
 
+
+    if(obj?.code !== code)
+        return res.json({
+            message: "Please enter the valid code."
+        }) 
+
+    
     if(!code || !token) return res.json({
         message:"Please provide the code sent as sms or try to sign in again."
     })
-
+    
+    let userInfo = await execute("select * from users where id = ?", [obj.user_id]);
+    
     return res.json({
-        message:"Ok",
+        message:"Ok", userInfo: userInfo[0],
         token: enCode({ id: code, type: "auth" })
     })
     
--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,55 @@
 const Joi = require('joi');
 
 const UserSchema = Joi.object({
-    phone_number: Joi.string()
+  first_name: Joi.string()
+    .trim()
+    .min(3)
+    .max(100)
+    .required()
+    .messages({
+      'string.base': 'First name must be a string',
+      'string.min': 'First name must be at least 3 characters long',
+      'string.max': 'First name must be less than or equal to 255 characters',
+      'any.required': 'First name is required',
+    }),
+
+  last_name: Joi.string()
+    .trim()
+    .min(3)
+    .max(100)
+    .required()
+    .messages({
+      'string.base': 'Last name must be a string',
+      'string.min': 'Last name must be at least 3 characters long',
+      'string.max': 'Last name must be less than or equal to 255 characters',
+      'any.required': 'Last name is required',
+    }),
+
+  surname: Joi.string()
+    .trim()
+    .min(3)
+    .max(100)
+    .optional()
+    .allow(null)
+    .messages({
+      'string.base': 'Surname must be a string',
+      'string.min': 'Surname must be at least 3 characters long',
+      'string.max': 'Surname must be less than or equal to 255 characters',
+    }),
+
+  email_address: Joi.string()
+    .trim()
+    .email({ tlds: { allow: false } })
+    .max(255)
+    .optional()
+    .allow(null)
+    .messages({
+      'string.base': 'Email Address must be a string',
+      'string.email': 'Email Address must be a valid email',
+      'string.max': 'Email Address must be less than or equal to 255 characters',
+    }),
+
+  phone_number: Joi.string()
     .trim()
     .pattern(/^\+?[0-9]{9,}$/)
     .required()
@@ -10,12 +58,25 @@ const UserSchema = Joi.object({
       'string.pattern.base': 'Phone Number must be at least 9 digits long and may start with a + sign',
       'any.required': 'Phone Number is required',
     }),
-  
-    // password: Joi.string().trim().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{5,30}$')).required().messages({
-    //       'string.pattern.base': 'Password must contain at least 5 characters, one uppercase letter, one lowercase letter, and one numeric value or special character',
-    //       'string.empty': 'Password cannot be an empty field',
-    //       'any.required': 'Password is a required field'
-    // })
+
+    birth_date: Joi.date()
+    .optional()
+    .allow(null)
+    .messages({
+      'date.base': 'Birthdate must be a valid date',
+    }),
+
+  gender: Joi.string()
+    .trim()
+    .optional()
+    .allow(null)
+    .valid('Male', 'Female')
+    .messages({
+      'string.base': 'Surname must be a string',
+      'string.min': 'Surname must be at least 3 characters long',
+      'string.max': 'Surname must be less than or equal to 255 characters',
+    }),
+
   });
 
 module.exports = UserSchema;
